b6a29e065f48dcf90cd2d5a0ec026880ae185c32
завадская.вера <.@TERM1>
2025-01-14 14:35:34 +0300

    Добавьте файлы проекта.

---
+++ ValuteConverter/View/Window/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ValuteConverter.Model;
+using static System.Net.WebRequestMethods;
+
+namespace ValuteConverter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string JSON_PATH = "https://www.cbr-xml-daily.ru/daily_json.js";
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadCurrencyAsync();
+        }
+
+        async Task LoadCurrencyAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(JSON_PATH);
+
+            if(!string.IsNullOrEmpty(response))
+            {
+                Currency currency =  JsonConvert.DeserializeObject<Currency>(response);
+
+                CurrentcyLV.ItemsSource = currency.Valute.Values;
+            }
+        }
+    }
+}
